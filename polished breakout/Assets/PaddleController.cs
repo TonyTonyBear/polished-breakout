f17fd5645f9c1d155adaa48da479da4d5acb7c99
Tony Barrett <ajbarrettx@gmail.com>
2020-08-01 09:21:15 -0400

    Add keyboard controls to paddle

---
+++ polished breakout/Assets/PaddleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PolishedBreakout
+{
+    public class PaddleController : MonoBehaviour
+    {
+        void Update()
+        {
+            Vector3 updatedPosition = Vector3.zero;
+            float moveSpeed = 5f;
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                // Move left.
+                updatedPosition.x -= Time.deltaTime * moveSpeed;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                // Move right.
+                updatedPosition.x += Time.deltaTime * moveSpeed;
+            }
+
+            transform.localPosition += updatedPosition;
+
+        }
+    }
+}
+
