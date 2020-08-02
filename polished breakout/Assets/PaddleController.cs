4f614feaaf38db615c63c8dfb84950fda64bbbfc
Tony Barrett <ajbarrettx@gmail.com>
2020-08-02 11:16:15 -0400

    Add logic that prevented the paddle from moving beyond the walls

--- polished breakout/Assets/PaddleController.cs
+++ polished breakout/Assets/PaddleController.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 namespace PolishedBreakout
 {
+    public enum CollisionState { LEFT, RIGHT, NONE }
+
     public class PaddleController : MonoBehaviour
     {
+        private CollisionState currentCollisionState;
+        private BoxCollider2D boxCollider;
+
+        private void OnEnable()
+        {
+            currentCollisionState = CollisionState.NONE;
+            boxCollider = GetComponent<BoxCollider2D>();
+        }
+
         void Update()
         {
             Vector3 updatedPosition = Vector3.zero;
@@ -14,16 +25,57 @@ namespace PolishedBreakout
             if (Input.GetKey(KeyCode.A))
             {
                 // Move left.
-                updatedPosition.x -= Time.deltaTime * moveSpeed;
+                updatedPosition.x -= currentCollisionState == CollisionState.LEFT ? 0f : Time.deltaTime * moveSpeed;
             }
             else if (Input.GetKey(KeyCode.D))
             {
                 // Move right.
-                updatedPosition.x += Time.deltaTime * moveSpeed;
+                updatedPosition.x += currentCollisionState == CollisionState.RIGHT ? 0f : Time.deltaTime * moveSpeed;
             }
 
             transform.localPosition += updatedPosition;
 
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            WallCollision wall = collision.gameObject.GetComponent<WallCollision>();
+
+            if (wall != null)
+            {
+                // Determine the direction of the collision to know what our wall collision is.
+                if (collision.transform.position.x > transform.position.x)
+                {
+                    currentCollisionState = CollisionState.RIGHT;
+
+                    Vector3 updatedPosition = transform.position;
+                    updatedPosition.x = wall.transform.position.x -
+                                        wall.boxCollider.bounds.extents.x -
+                                        boxCollider.bounds.extents.x;
+
+                    transform.position = updatedPosition;
+                }
+                else
+                {
+                    currentCollisionState = CollisionState.LEFT;
+
+                    Vector3 updatedPosition = transform.position;
+
+                    updatedPosition.x = wall.transform.position.x +
+                                        wall.boxCollider.bounds.extents.x +
+                                        boxCollider.bounds.extents.x;
+
+                    transform.position = updatedPosition;
+                }
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.GetComponent<WallCollision>() != null)
+            {
+                currentCollisionState = CollisionState.NONE;
+            }
         }
     }
 }
---
+++ polished breakout/Assets/WallCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PolishedBreakout
+{
+    public class WallCollision : MonoBehaviour
+    {
+        private BoxCollider2D _boxCollider;
+        public BoxCollider2D boxCollider { get { return _boxCollider; } }
+
+        private void OnEnable()
+        {
+            _boxCollider = GetComponent<BoxCollider2D>();
+        }
+    }
+}
+
