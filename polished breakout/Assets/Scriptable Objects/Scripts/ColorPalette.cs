17d0d8c0e1d446e0446beb71e9dd885eb5148240
Tony Barrett <ajbarrettx@gmail.com>
2020-08-12 11:46:47 -0400

    Resolves #20

---
+++ polished breakout/Assets/Scriptable Objects/Scripts/ColorPalette.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Color Palette", menuName = "Breakout/Color Palette", order = 0)]
+public class ColorPalette : ScriptableObject
+{
+    public Color walls, paddle, bricks, ball, background;
+}
---
+++ polished breakout/Assets/Scripts/ColorPaletteSetter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPaletteSetter : MonoBehaviour
+{
+    [SerializeField] private List<ColorPalette> colorPalettes;
+    private int currentPaletteIndex = 0;
+
+    [SerializeField] private SpriteRenderer ball, paddle, background;
+    [SerializeField] private List<SpriteRenderer> bricks, walls;
+
+    private void OnEnable()
+    {
+        ColorPalette currentPalette = colorPalettes[currentPaletteIndex];
+
+        ball.color = currentPalette.ball;
+        paddle.color = currentPalette.paddle;
+        background.color = currentPalette.background;
+
+        foreach (SpriteRenderer brick in bricks)
+            brick.color = currentPalette.bricks;
+
+        foreach (SpriteRenderer wall in walls)
+            wall.color = currentPalette.walls;
+    }
+}
