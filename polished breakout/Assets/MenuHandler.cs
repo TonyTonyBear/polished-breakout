2fe09846484cbaa1c103aa09b2019d8c50f86104
Tony Barrett <ajbarrettx@gmail.com>
2020-08-18 21:46:34 -0400

    Resolves #26 #28 #34

---
+++ polished breakout/Assets/MenuHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void TransitionToGameplay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void TransitionToSettings()
+    {
+        Debug.Log("Show settings");
+    }
+
+    public void TransitionToStats()
+    {
+        Debug.Log("Show Stats");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
